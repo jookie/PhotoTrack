b018594bc97d7b682187eb7b57af26cdc396b1d5
Ian Macartney <ianhmacartney@gmail.com>
2023-07-05 20:12:30 -0700

    update Clerk b/c of global issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,19 @@
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
-import { useEffect, StrictMode } from "react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
-
-function ClerkConvexAdapter() {
-  const { getToken, isSignedIn } = useAuth();
-
-  useEffect(() => {
-    if (isSignedIn) {
-      convex.setAuth(async () =>
-        getToken({ template: "convex", skipCache: true })
-      );
-    } else {
-      convex.clearAuth();
-    }
-  }, [getToken, isSignedIn]);
-  return null;
-}
 
 ReactDOM.render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-      <ConvexProvider client={convex}>
-        <ClerkConvexAdapter />
+      <ConvexProviderWithClerk client={convex}>
         <App />
-      </ConvexProvider>
+      </ConvexProviderWithClerk>
     </ClerkProvider>
   </StrictMode>,
   document.getElementById("root")
